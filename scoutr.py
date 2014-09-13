3e8198c40aa3a097ba53b10a4b0ffc4e3fafdaaf
Gabe Fernando <gef756@gmail.com>
2014-09-13 16:43:08 -0400

    CLN: rewrite templates

--- scoutr.py
+++ scoutr.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello(name=None):
     return render_template('index.html', name=name)
 
+@app.route('/info/')
+def info_collect():
+    return render_template('info.html')
+
 if __name__ == '__main__':
-	app.debug = True
-	app.run()
+    app.debug = True
+    app.run()
