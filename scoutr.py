2e83bbbb340fa9def98094f9f2464d6b248fc267
Gabe Fernando <gef756@gmail.com>
2014-09-13 12:54:19 -0400

    ENH: add basic templates

--- scoutr.py
+++ scoutr.py
@@ -1,8 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello world!'
+def hello(name=None):
+    return render_template('index.html', name=name)
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
