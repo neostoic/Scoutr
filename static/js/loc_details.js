22706ff9e66ce3f6244f81b1bffa4dcab3e7ca17
AB5 <newbox.me@gmail.com>
2014-09-14 11:50:49 -0400

    ENH: add watchlist functionality

--- static/js/loc_details.js
+++ static/js/loc_details.js
@@ -30,8 +30,9 @@ function setVenueClick(){
 		if (menu[0][3])
 			$('#venue-details .location-img').append('<img src="' + menu[0][3] + '">')
 		for (i = 0; i < menu.length; i++){
-			menu_block = '<li><span class="prod_name">' + menu[i][0] + '</span> <span class="prod_price">($'+ menu[i][2] +')<br><span class="prod_descr">' + menu[i][1] + '</span>';
+			menu_block = '<li class="item"><span class="prod_name">' + menu[i][0] + '</span> <span class="prod_price">($'+ menu[i][2] +')<br><span class="prod_descr">' + menu[i][1] + '</span>';
 			$('#venue-details .venue_prods ul').append(menu_block);
 		}
+		makeItemsDragable();
 	});
 }
